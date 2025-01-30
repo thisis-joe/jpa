aaf5b6ba38755ef251a8f76d902e8d3eba9d57f2
joseph <wdg0434@gmail.com>
2025-01-30 17:25:29 +0900

    feat :#41 add transactional on PostServiceTest

--- src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/example/jpa/domain/post/post/service/PostServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -13,6 +14,7 @@ public class PostServiceTest {
     private PostService postService;
     @Test
     @DisplayName("글 2개 작성")
+    @Transactional
     public void t1(){
         postService.write("title1","body1");
         postService.write("title2","body2");
